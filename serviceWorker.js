eff0fdc31eb74d1c68bea6a4cc258dce5b60e32d
Jonathan B <45127993+Crasher508@users.noreply.github.com>
2023-11-03 19:27:12 +0100

    update + add webapp beta

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticDevCrasher508 = "dev-crasher508-unofficial-dvb-dm-v1";
+const assets = [
+    "/",
+    "/index.html",
+    "/icons/Icon-192.png",
+    "/icons/Icon-maskable-192.png",
+    "/icons/Icon-512.png",
+    "/icons/Icon-maskable-512.png"
+];
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevCrasher508).then(cache => {
+            cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
+});
